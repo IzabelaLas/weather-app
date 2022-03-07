5c5ddc7b6d466a2edbe6f39665882f3734843810
IzabelaLas <izabela.laszczuk@gmail.com>
2022-03-07 22:19:56 +0100

    display forecast data

--- inde.js
+++ inde.js
@@ -20,27 +20,41 @@ function changeDay(date) {
   let day = days[dayIndex];
   return `${day}, ${hours}:${minutes}`;
 }
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
 function displayForecast(response) {
-  console.log(response.data.daily);
+  let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
   let days = ["Wed", "Thu", "Fri", "Sat", "Sun"];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `  
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `  
         <div class="col-2">
-          <div class="weather-forecast-date">${day}</div>
-          <div class="weather-forecast-emoji">
-          <i class="fas fa-cloud-showers-heavy rain"></i>
-          </div>
+          <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+         <img src="http://openweathermap.org/img/wn/${
+           forecastDay.weather[0].icon
+         }@2x.png" alt="" width="42" />
           <div class="weather-forecast-temperatures">
-           <span class="weather-forecast-temperature-max">3°</span>
-           <span class="weather-forecast-temperature-min">-2°</span>
+           <span class="weather-forecast-temperature-max">${Math.round(
+             forecastDay.temp.max
+           )}°</span>
+           <span class="weather-forecast-temperature-min">${Math.round(
+             forecastDay.temp.min
+           )}°</span>
            </div>
           </div>`;
+    }
   });
+
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
 }
@@ -230,4 +244,4 @@ currentButton.addEventListener("click", currentPosition);
 let searchButton = document.querySelector("#search-box");
 searchButton.addEventListener("submit", searchedPosition);
 
-searchCity("San Francisco");
+searchCity("Warsaw");
